1aa4d8910843d11242da4b979d00382097e6862e
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-08-20 14:40:03 +0300

    new & create products added

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -13,6 +13,25 @@ const index = async (req, res) => {
   }
 };
 
+const newProduct = (req, res) => {
+  res.render('products/new.ejs');
+};
+
+const create = async (req, res) => {
+  try {
+    req.body.owner = req.session.user._id;
+
+    await Product.create(req.body);
+
+    res.redirect('/products');
+  } catch (error) {
+    console.log(error);
+    res.send('Unable to create product.');
+  }
+};
+
 module.exports = {
   index,
+  new: newProduct,
+  create,
 };
--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const productsCtrl = require('../controllers/productsCtrl.js');
+const isSignedIn = require('../middleware/isSignedIn.js');
 
 router.get('/', productsCtrl.index);
+router.get('/new', isSignedIn, productsCtrl.new);
+router.post('/', isSignedIn, productsCtrl.create);
 
 module.exports = router;
