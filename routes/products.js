d2d7443070b60ff38057403258b0cfabf908d871
Maryam Hussain <202011514@stu.uob.edu.bh>
2026-08-23 10:17:13 +0300

    Update product images

--- routes/products.js
+++ routes/products.js
@@ -7,9 +7,11 @@ const isSignedIn = require('../middleware/isSignedIn.js');
 router.get('/', productsCtrl.index);
 router.get('/new', isSignedIn, productsCtrl.new);
 router.post('/', isSignedIn, productsCtrl.create);
+
 router.get('/:productId/edit', isSignedIn, productsCtrl.edit);
 router.put('/:productId', isSignedIn, productsCtrl.update);
 router.delete('/:productId', isSignedIn, productsCtrl.deleteProduct);
+
 router.get('/:productId', productsCtrl.show);
 
 module.exports = router;
